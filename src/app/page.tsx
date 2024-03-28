bb881be6e91f0804ac19ac3ef21fcdb4004d9eaa
achiewe <mamukashviliachi706@gmail.com>
2024-03-28 22:26:48 +0400

    change the title of the porject

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
-
 export default function Home() {
-  return;
+  return (
+    <div className="min-h-screen w-full flex flex-col justify-center items-center bg-[#1E213F]"></div>
+  );
 }
