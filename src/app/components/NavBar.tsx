66ddbe0c670a6116b61116cd5b18e464aabe888b
achiewe <mamukashviliachi706@gmail.com>
2024-03-29 15:18:07 +0400

    add the text color on the h3 tag

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -2,9 +2,21 @@ import React from "react";
 
 export default function NavBar() {
   return (
-    <div className="w-[327px] flex flex-row items-center gap-[10px] rounded-[20px] bg-[#161932] p-[5px]">
-      <div className="bg-[red] w-[106px] flex justify-center items-center h-[48px] rounded-[]">
-        <h3> pomodoro</h3>
+    <div className="w-[327px] flex flex-row items-center gap-[10px] rounded-[25px] bg-[#161932] p-[5px]">
+      <div className="bg-[red] w-[106px] flex justify-center items-center h-[48px] rounded-[25px]">
+        <h3 className="text-[12px] leading-[17.77px] font-bold text-[#D7E0FF]">
+          pomodoro
+        </h3>
+      </div>
+      <div className="bg-[red] w-[106px] flex justify-center items-center h-[48px] rounded-[25px]">
+        <h3 className="text-[12px] leading-[17.77px] font-bold text-[#D7E0FF]">
+          short break
+        </h3>
+      </div>
+      <div className="bg-[red] w-[106px] flex justify-center items-center h-[48px] rounded-[25px]">
+        <h3 className="text-[12px] leading-[17.77px] font-bold text-[#D7E0FF]">
+          long break
+        </h3>
       </div>
     </div>
   );
