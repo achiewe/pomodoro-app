dbdf816b8af84a6e7e8eff3fe95de2e5e89d94f8
achiewe <mamukashviliachi706@gmail.com>
2024-03-29 15:00:51 +0400

    create new component navbar to write what options have pomodoro

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,13 +4,8 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header className="flex flex-col gap-[20px] w-full justify-center items-center">
+    <header className="flex flex-col w-full justify-center items-center mb-[20px]">
       <Image src={logoSvg} alt="logo svg" />
-      <div className="w-[327px] flex flex-row items-center gap-[10px] rounded-[20px] bg-[#161932] p-[5px]">
-        <div className="bg-[red] w-[106px] flex justify-center items-center h-[48px]">
-          <h3> pomodoro</h3>
-        </div>
-      </div>
     </header>
   );
 }
