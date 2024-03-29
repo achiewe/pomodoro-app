b17a4d2352da039d9e6bf7d9788824152e78fc48
achiewe <mamukashviliachi706@gmail.com>
2024-03-29 15:01:43 +0400

    import navbar in the main page tsx component

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <div className="w-[327px] flex flex-row items-center gap-[10px] rounded-[20px] bg-[#161932] p-[5px]">
+      <div className="bg-[red] w-[106px] flex justify-center items-center h-[48px] rounded-[]">
+        <h3> pomodoro</h3>
+      </div>
+    </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import NavBar from "./components/NavBar";
 
 export default function Home() {
   return (
     <div className="min-h-screen w-full flex flex-col justify-center items-center bg-[#1E213F] px-[20px]">
       <Header />
+      <NavBar />
     </div>
   );
 }
