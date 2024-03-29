a24e65e1d533acb8ff0e1783dd9afdecbf62ab73
achiewe <mamukashviliachi706@gmail.com>
2024-03-29 14:59:44 +0400

    style the header component

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -6,6 +6,11 @@ export default function Header() {
   return (
     <header className="flex flex-col gap-[20px] w-full justify-center items-center">
       <Image src={logoSvg} alt="logo svg" />
+      <div className="w-[327px] flex flex-row items-center gap-[10px] rounded-[20px] bg-[#161932] p-[5px]">
+        <div className="bg-[red] w-[106px] flex justify-center items-center h-[48px]">
+          <h3> pomodoro</h3>
+        </div>
+      </div>
     </header>
   );
 }
