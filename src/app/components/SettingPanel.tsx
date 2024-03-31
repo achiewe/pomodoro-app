feefbedc92fc71dee6758dc5c37055c775cf0532
achiewe <mamukashviliachi706@gmail.com>
2024-03-31 11:28:55 +0400

    add the button to change the font of the text

--- src/app/components/SettingPanel.tsx
+++ src/app/components/SettingPanel.tsx
@@ -65,6 +65,23 @@ export default function SettingPanel() {
               </div>
             </div>
           </div>
+          <div className="w-full h-[1px] bg-[#E3E1E1] mt-6"></div>
+          <div className="flex flex-col md:flex-row md:justify-between items-center">
+            <h3 className="mt-6 text-[11px] md:text-sm text-[#161932] font-bold tracking-[5px] text-center">
+              FONT
+            </h3>
+            <div className="flex items-center gap-4 mt-4">
+              <button className="w-10 h-10 rounded-full text-base font-base flex items-center justify-center bg-[#EFF1FA] kumbh">
+                Aa
+              </button>
+              <button className="w-10 h-10 rounded-full text-base font-base flex items-center justify-center bg-[#EFF1FA] kumbh">
+                Aa
+              </button>
+              <button className="w-10 h-10 rounded-full text-base font-base flex items-center justify-center bg-[#EFF1FA] kumbh">
+                Aa
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
