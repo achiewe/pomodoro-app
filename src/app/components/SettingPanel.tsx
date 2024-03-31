498188f028e098d6e450f18387014423b2824ea1
achiewe <mamukashviliachi706@gmail.com>
2024-03-31 12:00:23 +0400

    add the color div to change content and timer color

--- src/app/components/SettingPanel.tsx
+++ src/app/components/SettingPanel.tsx
@@ -82,6 +82,19 @@ export default function SettingPanel() {
               </button>
             </div>
           </div>
+          <div className="w-full h-[1px] bg-[#E3E1E1] mt-6"></div>
+          <div className="flex flex-col md:flex-row md:justify-between items-center md:mt-6">
+            <h3 className="mt-6 md:mt-0 text-[11px] md:text-sm text-dark-blu font-bold tracking-[5px] text-center">
+              COLOR
+            </h3>
+            <div className="flex items-center gap-4 mt-4 md:mt-0">
+              <button className="border-none w-10 h-10 rounded-full  bg-[#F87070] flex items-center justify-center"></button>
+
+              <button className="border-none w-10 h-10 rounded-full  bg-[#70F3F8] flex items-center justify-center"></button>
+
+              <button className="border-none w-10 h-10 rounded-full  bg-[#D881F8] flex items-center justify-center"></button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
