936cf92ced6d9cd41be1ac6eefbde87e44633d1f
achiewe <mamukashviliachi706@gmail.com>
2024-03-31 12:35:09 +0400

    inster the main page tsx code in the provider to use redux

--- src/app/feature/store.ts
+++ src/app/feature/store.ts
@@ -10,3 +10,5 @@ const store = configureStore({
 export type RootState = {
   openSetting: SettingProps;
 };
+
+export default store;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,21 @@
+import { Provider } from "react-redux";
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
 import SettingPanel from "./components/SettingPanel";
 import Settings from "./components/Settings";
 import TimerDisplay from "./components/TimerDisplay";
+import store from "./feature/store";
 
 export default function Home() {
   return (
-    <div className="min-h-screen w-full flex flex-col justify-start items-center bg-[#1E213F] p-[30px] relative">
-      <Header />
-      <NavBar />
-      <TimerDisplay />
-      <Settings />
-      <SettingPanel />
-    </div>
+    <Provider store={store}>
+      <div className="min-h-screen w-full flex flex-col justify-start items-center bg-[#1E213F] p-[30px] relative">
+        <Header />
+        <NavBar />
+        <TimerDisplay />
+        <Settings />
+        <SettingPanel />
+      </div>
+    </Provider>
   );
 }
