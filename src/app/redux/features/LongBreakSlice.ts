93f583cae22113cb4a54ae2b14a1ef6f7ffaf9b8
achiewe <mamukashviliachi706@gmail.com>
2024-04-01 14:44:57 +0400

    create state for long break

---
+++ src/app/redux/features/LongBreakSlice.ts
@@ -0,0 +1,22 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+export interface LongBreakProps {
+  longBreak: string;
+}
+
+const initialState: LongBreakProps = {
+  longBreak: "15:00",
+};
+
+const LongBreakSlice = createSlice({
+  name: "longBreak",
+  initialState,
+  reducers: {
+    setLongBreak: (state, action: PayloadAction<string>) => {
+      state.longBreak = action.payload;
+    },
+  },
+});
+
+export const { setLongBreak } = LongBreakSlice.actions;
+export default LongBreakSlice.reducer;
