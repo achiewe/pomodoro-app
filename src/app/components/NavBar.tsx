d84eb61e2babb94a44de6bb62a631b6e36e7eb10
achiewe <mamukashviliachi706@gmail.com>
2024-04-01 13:15:12 +0400

    add the logic that if the panel option state value will change and become some other mode name that background color become red

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,10 +1,14 @@
 "use client";
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setPanelOption } from "../redux/features/PanelOptionSlice";
+import { RootState } from "../redux/store";
 
 export default function NavBar() {
   const dispatch = useDispatch();
+  const panelOption = useSelector(
+    (store: RootState) => store.panelOption.panelOption
+  );
 
   return (
     <div className="w-[327px] flex flex-row items-center gap-[10px] rounded-[25px] bg-[#161932] p-[5px]">
@@ -12,14 +16,18 @@ export default function NavBar() {
         onClick={() => {
           dispatch(setPanelOption("pomodoro"));
         }}
-        className="bg-[#161932] w-[106px] flex justify-center items-center h-[48px] rounded-[25px]"
+        className={`${
+          panelOption === "pomodoro" ? "bg-[red]" : "bg-[#161932]"
+        } w-[106px] flex justify-center items-center h-[48px] rounded-[25px]`}
       >
         <h3 className="text-[12px] leading-[17.77px] font-bold text-[#D7E0FF]">
           pomodoro
         </h3>
       </div>
       <div
-        className="bg-[#161932] w-[106px] flex justify-center items-center h-[48px] rounded-[25px]"
+        className={`${
+          panelOption === "short break" ? "bg-[red]" : "bg-[#161932]"
+        } w-[106px] flex justify-center items-center h-[48px] rounded-[25px]`}
         onClick={() => {
           dispatch(setPanelOption("short break"));
         }}
@@ -29,7 +37,9 @@ export default function NavBar() {
         </h3>
       </div>
       <div
-        className="bg-[#161932] w-[106px] flex justify-center items-center h-[48px] rounded-[25px]"
+        className={`${
+          panelOption === "long break" ? "bg-[red]" : "bg-[#161932]"
+        } w-[106px] flex justify-center items-center h-[48px] rounded-[25px]`}
         onClick={() => {
           dispatch(setPanelOption("long break"));
         }}
