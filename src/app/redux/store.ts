58dc44a4af39148c0ef79cd885d89b90d188ca7b
achiewe <mamukashviliachi706@gmail.com>
2024-04-01 13:25:46 +0400

    add the state in the store to use it in the project

--- src/app/redux/store.ts
+++ src/app/redux/store.ts
@@ -1,17 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import OpenSettingSlice, { SettingProps } from "./features/OpenSettingSlice";
 import PanelOptionSlice, { PanelProps } from "./features/PanelOptionSlice";
+import PomodoroSlice, { PomodoroProps } from "./features/PomodoroSlice";
 
 const store = configureStore({
   reducer: {
     openSetting: OpenSettingSlice,
     panelOption: PanelOptionSlice,
+    pomodoroTimer: PomodoroSlice,
   },
 });
 
 export type RootState = {
   openSetting: SettingProps;
   panelOption: PanelProps;
+  pomodoroTimer: PomodoroProps;
 };
 
 export default store;
