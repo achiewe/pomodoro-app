5a3abfbc26c6391e802d2d0eb6f442dfda3c9df1
achiewe <mamukashviliachi706@gmail.com>
2024-04-01 14:41:19 +0400

    create new state for show when will be short break

---
+++ src/app/redux/features/ShortBreakSlice.ts
@@ -0,0 +1,22 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+export interface ShortBreakProps {
+  shortBreak: string;
+}
+
+const initialState: ShortBreakProps = {
+  shortBreak: "25:00",
+};
+
+const ShortBreakSlice = createSlice({
+  name: "shortBreak",
+  initialState,
+  reducers: {
+    setShortBreak: (state, action: PayloadAction<string>) => {
+      state.shortBreak = action.payload;
+    },
+  },
+});
+
+export const { setShortBreak } = ShortBreakSlice.actions;
+export default ShortBreakSlice.reducer;
