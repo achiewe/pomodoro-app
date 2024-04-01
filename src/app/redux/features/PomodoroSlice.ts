b828750b15d4e4f48e7802543b40e05a1527e556
achiewe <mamukashviliachi706@gmail.com>
2024-04-01 13:25:05 +0400

    create new state for write pomodoro time in the state and then change it

---
+++ src/app/redux/features/PomodoroSlice.ts
@@ -0,0 +1,22 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+export interface PomodoroProps {
+  pomodoroTimer: string;
+}
+
+const initialState: PomodoroProps = {
+  pomodoroTimer: "25:00",
+};
+
+const PomodoroSlice = createSlice({
+  name: "pomodoroTimer",
+  initialState,
+  reducers: {
+    setPomodoroTimer: (state, action: PayloadAction<string>) => {
+      state.pomodoroTimer = action.payload;
+    },
+  },
+});
+
+export const { setPomodoroTimer } = PomodoroSlice.actions;
+export default PomodoroSlice.reducer;
