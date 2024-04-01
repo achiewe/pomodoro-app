d3f408f40e8d24d1e7f2471ec92f78c5443bdfa8
achiewe <mamukashviliachi706@gmail.com>
2024-04-01 13:02:35 +0400

    add the onclick in the NavBar to change the state value when onclick on the various option of the timer

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,19 +1,39 @@
+"use client";
 import React from "react";
+import { useDispatch } from "react-redux";
+import { setPanelOption } from "../redux/features/PanelOptionSlice";
 
 export default function NavBar() {
+  const dispatch = useDispatch();
+
   return (
     <div className="w-[327px] flex flex-row items-center gap-[10px] rounded-[25px] bg-[#161932] p-[5px]">
-      <div className="bg-[#161932] w-[106px] flex justify-center items-center h-[48px] rounded-[25px]">
+      <div
+        onClick={() => {
+          dispatch(setPanelOption("pomodoro"));
+        }}
+        className="bg-[#161932] w-[106px] flex justify-center items-center h-[48px] rounded-[25px]"
+      >
         <h3 className="text-[12px] leading-[17.77px] font-bold text-[#D7E0FF]">
           pomodoro
         </h3>
       </div>
-      <div className="bg-[#161932] w-[106px] flex justify-center items-center h-[48px] rounded-[25px]">
+      <div
+        className="bg-[#161932] w-[106px] flex justify-center items-center h-[48px] rounded-[25px]"
+        onClick={() => {
+          dispatch(setPanelOption("short break"));
+        }}
+      >
         <h3 className="text-[12px] leading-[17.77px] font-bold text-[#D7E0FF]">
           short break
         </h3>
       </div>
-      <div className="bg-[#161932] w-[106px] flex justify-center items-center h-[48px] rounded-[25px]">
+      <div
+        className="bg-[#161932] w-[106px] flex justify-center items-center h-[48px] rounded-[25px]"
+        onClick={() => {
+          dispatch(setPanelOption("long break"));
+        }}
+      >
         <h3 className="text-[12px] leading-[17.77px] font-bold text-[#D7E0FF]">
           long break
         </h3>
