35f3c40be1a997d8a6aa0c723bb4cdfd5a67c54f
achiewe <mamukashviliachi706@gmail.com>
2024-04-01 13:09:58 +0400

    add state panelOption in the store file to use it in the project

--- src/app/redux/store.ts
+++ src/app/redux/store.ts
@@ -1,14 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import OpenSettingSlice, { SettingProps } from "./features/OpenSettingSlice";
+import PanelOptionSlice, { PanelProps } from "./features/PanelOptionSlice";
 
 const store = configureStore({
   reducer: {
     openSetting: OpenSettingSlice,
+    panelOption: PanelOptionSlice,
   },
 });
 
 export type RootState = {
   openSetting: SettingProps;
+  panelOption: PanelProps;
 };
 
 export default store;
