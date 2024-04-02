74c0976e5663a14f7ff07e9afcb624e8eb10c114
achiewe <mamukashviliachi706@gmail.com>
2024-04-02 18:11:35 +0400

    add the onclick on the second and third button to change the value state and become roboto or space mono

--- src/app/components/SettingsFont.tsx
+++ src/app/components/SettingsFont.tsx
@@ -1,6 +1,9 @@
+"use client";
+import { useDispatch } from "react-redux";
 import { setFontContent } from "../redux/features/FontSettingSlice";
 
 export default function SettingsFont() {
+  const dispatch = useDispatch();
   return (
     <div className="flex flex-col md:flex-row md:justify-between items-center">
       <h3 className="mt-6 text-[11px] md:text-sm text-[#161932] font-bold tracking-[5px] text-center">
@@ -10,15 +13,25 @@ export default function SettingsFont() {
         <button
           className="w-10 h-10 rounded-full text-base font-Kumbh flex items-center justify-center bg-[#EFF1FA]"
           onClick={() => {
-            setFontContent("Kumbh");
+            dispatch(setFontContent("Kumbh"));
           }}
         >
           Aa
         </button>
-        <button className="w-10 h-10 rounded-full text-base font-Roboto flex items-center justify-center bg-[#EFF1FA]">
+        <button
+          className="w-10 h-10 rounded-full text-base font-Roboto flex items-center justify-center bg-[#EFF1FA]"
+          onClick={() => {
+            dispatch(setFontContent("Roboto"));
+          }}
+        >
           Aa
         </button>
-        <button className="w-10 h-10 rounded-full text-base font-SpaceMono flex items-center justify-center bg-[#EFF1FA]">
+        <button
+          className="w-10 h-10 rounded-full text-base font-SpaceMono flex items-center justify-center bg-[#EFF1FA]"
+          onClick={() => {
+            dispatch(setFontContent("SpaceMono"));
+          }}
+        >
           Aa
         </button>
       </div>
