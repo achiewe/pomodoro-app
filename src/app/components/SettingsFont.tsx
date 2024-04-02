024957bc4d665000f43b378fca3ce39264be0172
achiewe <mamukashviliachi706@gmail.com>
2024-04-02 18:58:50 +0400

    add the logic about font to change the background color of the button

--- src/app/components/SettingsFont.tsx
+++ src/app/components/SettingsFont.tsx
@@ -25,7 +25,9 @@ export default function SettingsFont() {
           Aa
         </button>
         <button
-          className="w-10 h-10 rounded-full text-base font-Roboto flex items-center justify-center bg-[#EFF1FA]"
+          className={`w-10 h-10 rounded-full text-base font-Roboto flex items-center justify-center ${
+            fontContent === "Roboto" ? "bg-[#161932]" : "bg-[#EFF1FA]"
+          }`}
           onClick={() => {
             dispatch(setFontContent("Roboto"));
           }}
