9b2e15e1ebbe3b45f79baa36a4ffe41d47fa809a
achiewe <mamukashviliachi706@gmail.com>
2024-04-02 18:09:35 +0400

    add the onclick on the button to change the font and become font kumbh

--- src/app/components/SettingsFont.tsx
+++ src/app/components/SettingsFont.tsx
@@ -1,3 +1,5 @@
+import { setFontContent } from "../redux/features/FontSettingSlice";
+
 export default function SettingsFont() {
   return (
     <div className="flex flex-col md:flex-row md:justify-between items-center">
@@ -5,7 +7,12 @@ export default function SettingsFont() {
         FONT
       </h3>
       <div className="flex items-center gap-4 mt-4">
-        <button className="w-10 h-10 rounded-full text-base font-Kumbh flex items-center justify-center bg-[#EFF1FA]">
+        <button
+          className="w-10 h-10 rounded-full text-base font-Kumbh flex items-center justify-center bg-[#EFF1FA]"
+          onClick={() => {
+            setFontContent("Kumbh");
+          }}
+        >
           Aa
         </button>
         <button className="w-10 h-10 rounded-full text-base font-Roboto flex items-center justify-center bg-[#EFF1FA]">
