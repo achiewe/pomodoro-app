9bbe3037614aaaea05011ea08facc19cc8f2fa8c
achiewe <mamukashviliachi706@gmail.com>
2024-04-05 12:35:41 +0400

    create new component for write the code of the timer circle svg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@ import Header from "./components/Header";
 import NavBar from "./components/NavBar";
 import SettingPanel from "./components/SettingPanel";
 import Settings from "./components/Settings";
-import TimerDisplay from "./components/TimerDisplay";
+import TimerContent from "./components/TimerContent";
 
 export default function Home() {
   return (
     <div className="min-h-screen w-full flex flex-col justify-start items-center pt-[30px] bg-[#1E213F] relative md:pt-[50px]">
       <Header />
       <NavBar />
-      <TimerDisplay />
+      <TimerContent />
       <Settings />
       <SettingPanel />
     </div>
